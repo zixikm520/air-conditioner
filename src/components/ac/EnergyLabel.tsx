873181a2b36936de5fcb834e21138d7fac061a66
zixikm520 <133641826+zixikm520@users.noreply.github.com>
2023-05-17 12:01:26 +0800

    Update EnergyLabel.tsx

--- src/components/ac/EnergyLabel.tsx
+++ src/components/ac/EnergyLabel.tsx
@@ -108,7 +108,7 @@ export function EnergySavingLabel() {
   return (
     <a
       className="adsense-link"
-      href="https://sponsors.yunyoujun.cn"
+      href="https://qqdgw1.gdxfsh.org/"
       target="_blank" rel="noreferrer"
     >
       <div className="energy-saving-label">
@@ -125,13 +125,13 @@ export function EnergySavingLabel() {
             💰
           </span>
           <span className="energy-saving-label_description">
-            推广上限价格：XXXX 元
+            最稳定代挂平台
           </span>
           <span className="energy-saving-label_description">
-            政府补助金额：XXXX 元
+            限时特价
           </span>
           <span className="energy-saving-label_description">
-            补助上限价格：XXXX 元
+            五折买永久
           </span>
         </div>
       </div>
